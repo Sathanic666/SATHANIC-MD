31c00dd881bebac85ccceb9883fce3d2fee4acb6
❇️ 𝐒𝐀𝐓𝐇𝐀𝐍𝐈𝐂 ❇️ <uicresher@gmail.com>
2024-10-06 19:34:40 +0530

    𝐒𝐀𝐓𝐇𝐀𝐍𝐈𝐂

---
+++ pair.js
@@ -0,0 +1,97 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Malvin_King,    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function MALVIN_KING_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_SATHAINIC = Sathanic({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Malvin_King.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Sathanic.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Sathanic.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Sathanic.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_sathanic.sendMessage(Pair_Code_By_Sathanic.user.id, { text: '' + b64data });
+
+               let SATHANIC_TEXT = `
+┏━━━━━━━━━━━━━━
+┃ᴍᴀʟᴠɪɴ-ᴍᴅ SESSION IS 
+┃SUCCESSFULLY
+┃CONNECTED ⚙️✔️
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❶ || Creator = 𖥘⚡ SATHANIC⚡𖥘
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❷ || https://whatsapp.com/channel/0029VamP2wJ9Gv7T2gOyln1G
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❸ || Owner = https://wa.me/9778158839
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❺ || Bot Repo = https://github.com/Sathanic666
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❻ || YouTube = https://youtube.com/@sathanic-f2p?si=FFRfk_Jo-0XxH4eG
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+©2024-2099 ᴍᴀʟᴠɪɴ-ᴋɪɴɢ_`
+ await Pair_Code_By_Sathanic.sendMessage(Pair_Code_By_Sathanic.user.id,{text:SATHANIC},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Malvin_King.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    MALVIN_KING_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await SATHANIC_PAIR_CODE()
+});
+module.exports = router
